4e6d50d9177ebb6079a5435ffe0024b0f4238db2
463623935 <463623935@qq.com>
2020-05-07 23:25:16 +0800

    init

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsConfigControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsConfigControllerApi.java
@@ -10,5 +10,6 @@ import io.swagger.annotations.ApiOperation;
 @Api(value="cms配置管理接口",description = "cms配置管理接口，提供数据模型的管理、查询接口")
 public interface CmsConfigControllerApi {
     @ApiOperation("根据id查询CMS配置信息")
-    public CmsConfig getmodel(String id);
+    public CmsConfig getmodel(String id);//测试git
+
 }
